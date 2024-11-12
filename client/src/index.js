21207d30c72a2e19140b89c609b744ce2dfa15dd
Alex Lee <alexanderlee014@gmail.com>
2024-11-11 21:56:28 -0800

    Added some java files

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
 var submitBtn = document.querySelector('.btn');
+// var emailAddr = document.querySelector('') // Get email Addr by querySelector
+// var password = document.querySelector('') //Get password by querySelector
+
+
+//Event listener that sends email addr and pass to server side code after submit Btn
+let Submit = () =>{ 
+
+};
