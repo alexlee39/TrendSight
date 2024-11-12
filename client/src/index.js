de359b702afb580e46aafb2e91c5a77968fb59fc
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-11-11 21:47:24 -0800

    finished up front-end login and register popups with basic animations, need to work with getting user data for registering

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,28 @@
 var submitBtn = document.querySelector('.btn');
+
+// swapping between register and login 
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+//swapping between login pop and close
+const loginPopup = document.querySelector('.login-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+//clicking links adds "active" and CSS responds accordingly (sets transfromX to 400,0, and -400)
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+//clicking links adds "active-popup" and CSS responds accordingly (simply sets scale to 0 and 1)
+loginPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
