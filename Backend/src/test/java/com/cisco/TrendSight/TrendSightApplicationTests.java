54e6ebd3636dff0274eb99323599331c3da96125
Alex Lee <alexanderlee014@gmail.com>
2025-01-10 22:33:55 -0800

    Started Springboot Application using Spring Initializer

---
+++ Backend/src/test/java/com/cisco/TrendSight/TrendSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.TrendSight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
