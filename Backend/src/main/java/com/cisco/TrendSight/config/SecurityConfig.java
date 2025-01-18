aceed31d92abd9255c48825544a06a10582b116b
alexa <alexanderlee014@gmail.com>
2025-01-17 21:32:51 -0800

    Initialized lots of Authentication Dependencies including Bcrypt and UserAuthManager(DAO). Created a custom security chain that explains which endpoints are being permitted and other security options

---
+++ Backend/src/main/java/com/cisco/TrendSight/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.cisco.TrendSight.config;
+
+import com.cisco.TrendSight.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final MyUserDetailService userDetailService;
+
+    public SecurityConfig(MyUserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable) // same as: .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests((authorize) -> authorize
+//                .requestMatchers("/admin").hasRole("ADMIN") // Only Admins can visit /admin
+//                    .requestMatchers("")
+                //Permit All Users + Anonymous Users(ie. Users w/o acc) to visit page "/" and all pages after /article
+                .requestMatchers("/user","/login","/register","/article/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults())
+            .build();
+        }
+
+    @Bean
+    public MyUserDetailService userDetailsService() {
+        return userDetailService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(passwordEncoder());
+        provider.setUserDetailsService(userDetailService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
