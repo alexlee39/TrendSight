4c66095e84db3a7586c834723924d01fd2042934
alexa <alexanderlee014@gmail.com>
2025-01-27 15:50:27 -0800

    Added some more default, loaded data for more testing/debugging purposes

--- Backend/src/main/java/com/cisco/TrendSight/config/LoadDB.java
+++ Backend/src/main/java/com/cisco/TrendSight/config/LoadDB.java
@@ -25,9 +25,14 @@ public class LoadDB {
   CommandLineRunner preloadDB(ArticleRepository repository, MyUserRepository myUserRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Article("Article 1", "Article 1 Body", "Bob Ross")));
-      log.info("Preloading " + repository.save(new Article("Article 2", "Article 2 Body", "Joe Biden")));
-      log.info("Preloading User: " + myUserRepository.save(new MyUser("test@gmail.com",passwordEncoder.encode("test"))));
+        log.info("Preloading User: {}", myUserRepository.save(new MyUser("test@gmail.com", passwordEncoder.encode("test"))));
+        log.info("Preloading User: {}", myUserRepository.save(new MyUser("league@gmail.com", passwordEncoder.encode("oflegends"))));
+
+//      MyUser myUser =
+        log.info("Preloading Article: {}", repository.save(new Article("Article 1", "Article 1 Body", "Bob Ross", myUserRepository.findByEmail("test@gmail.com").get())));
+        log.info("Preloading Article: {}", repository.save(new Article("Article 2", "Article 2 Body", "Joe Biden", myUserRepository.findByEmail("test@gmail.com").get())));
+        log.info("Preloading Article: {}", repository.save(new Article("I LOVE LEAGUE", "Article 2 Body", "Tyler1", myUserRepository.findByEmail("league@gmail.com").get())));
+
     };
   }
 }
