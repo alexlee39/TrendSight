577363a58cf3ae01a6e6062b6ba6abd6c47c7ff9
alexa <alexanderlee014@gmail.com>
2025-01-27 15:55:22 -0800

    Updated POST, PUT, DELETE articles that authorizes and checks if current User owns/created the article themselves before editing. Add a method that allows User to get all articles they have created.

--- Backend/src/main/java/com/cisco/TrendSight/controller/ArticleController.java
+++ Backend/src/main/java/com/cisco/TrendSight/controller/ArticleController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.cisco.TrendSight.dto.ArticleDto;
+import com.cisco.TrendSight.model.MyUser;
+import com.cisco.TrendSight.service.MyUserDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,54 +27,88 @@ import com.cisco.TrendSight.repository.ArticleRepository;
 @RestController
 public class ArticleController {
     
-    private final ArticleRepository repository;
+    private final ArticleRepository articleRepository;
+    private final MyUserDetailService myUserDetailService;
 
-    public ArticleController(ArticleRepository repository) {
-        this.repository = repository;
+    public ArticleController(ArticleRepository articleRepository, MyUserDetailService myUserDetailService) {
+        this.articleRepository = articleRepository;
+        this.myUserDetailService = myUserDetailService;
     }
 
     @GetMapping("/article")
     public List<Article> findAllArticles(){
-        return repository.findAll();
+        return articleRepository.findAll();
     }
 
     @GetMapping("/article/{id}")
     public Article getArticleById(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(() -> new ArticleNotFoundException(id));
+        return articleRepository.findById(id).orElseThrow(() -> new ArticleNotFoundException(id));
     }
+
+    @PreAuthorize("hasRole('ROLE_AUTHOR')")
+    @GetMapping("/user/article")
+    public ResponseEntity<List<Article>> findAllArticlesFromUser(Authentication authentication){
+//        MyUser myUser = myUserDetailService.getUserFromId(userId);
+//        if(myUser == null){
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        String email = authentication.getName();
+//        if(!email.equals(myUser.getEmail())){
+//            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+//        }
+        String email = authentication.getName();
+        MyUser myUser = myUserDetailService.getUserFromEmail(email);
+        if(myUser == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(articleRepository.findAllByMyUser(myUser));
+    }
+
     @PreAuthorize("hasRole('ROLE_AUTHOR')")
     @PostMapping("/article")
-    public ResponseEntity<Article> postArticle(@RequestBody ArticleDto newArticle) {
-        Article article = new Article(newArticle.getTitle(),newArticle.getBody(), newArticle.getAuthor());
-        repository.save(article);
+    public ResponseEntity<Article> postArticle(@RequestBody ArticleDto newArticle, Authentication authentication) {
+        String email = authentication.getName();
+        MyUser myUser = myUserDetailService.getUserFromEmail(email);
+        Article article = new Article(newArticle.getTitle(),newArticle.getBody(), newArticle.getAuthor(),myUser);
+        articleRepository.save(article);
         return new ResponseEntity<>(article,HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasRole('ROLE_AUTHOR')")
     @PutMapping("/article/{id}")
-    public ResponseEntity<Article> updateArticle(@RequestBody ArticleDto newArticle, @PathVariable Long id){
+    public ResponseEntity<Article> updateArticle(@RequestBody ArticleDto newArticle, @PathVariable Long id, Authentication authentication){
 
-        Optional<Article> article = repository.findById(id);
-        if(article.isPresent()){
-            Article updatedArticle = article.get();
-            updatedArticle.setTitle(newArticle.getTitle());
-            updatedArticle.setBody(newArticle.getBody());
-            updatedArticle.setAuthor(newArticle.getAuthor());
-            updatedArticle.setUpdatedDate();
-            repository.save(updatedArticle);
-            return ResponseEntity.ok(updatedArticle);
+        Optional<Article> article = articleRepository.findById(id);
+        String email = authentication.getName();
+        MyUser myUser = myUserDetailService.getUserFromEmail(email);
+        if(article.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else if(!article.get().getMyUser().equals(myUser)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Article updatedArticle = article.get();
+        updatedArticle.setTitle(newArticle.getTitle());
+        updatedArticle.setBody(newArticle.getBody());
+        updatedArticle.setAuthor(newArticle.getAuthor());
+        updatedArticle.setUpdatedDate();
+        articleRepository.save(updatedArticle);
+        return ResponseEntity.ok(updatedArticle);
 
     }
 
     @PreAuthorize("hasRole('ROLE_AUTHOR')")
     @DeleteMapping("/article/{id}")
-    public ResponseEntity<String> deleteArticle(@PathVariable Long id){
-        if (!repository.existsById(id)) {
+    public ResponseEntity<String> deleteArticle(@PathVariable Long id, Authentication authentication){
+        String email = authentication.getName();
+        MyUser myUser = myUserDetailService.getUserFromEmail(email);
+        if (articleRepository.findById(id).isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        repository.deleteById(id);
+        else if(!articleRepository.findById(id).get().getMyUser().equals(myUser)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        articleRepository.deleteById(id);
         return new ResponseEntity<>("Article Deleted",HttpStatus.ACCEPTED);
     }
 }
