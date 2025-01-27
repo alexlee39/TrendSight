f2624ab6083c71288db366baacbf20b5a12258c4
alexa <alexanderlee014@gmail.com>
2025-01-27 15:50:00 -0800

    Add a query method to ArticleRepository to get all articles pertaining to a User Object

--- Backend/src/main/java/com/cisco/TrendSight/repository/ArticleRepository.java
+++ Backend/src/main/java/com/cisco/TrendSight/repository/ArticleRepository.java
@@ -1,9 +1,12 @@
 package com.cisco.TrendSight.repository;
 
+import com.cisco.TrendSight.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cisco.TrendSight.model.Article;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long>{
-    
+    List<Article> findAllByMyUser(MyUser myUser);
 }
