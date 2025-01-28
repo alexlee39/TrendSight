43a488158546364d445867d186a78bedda056980
alexa <alexanderlee014@gmail.com>
2025-01-27 20:34:33 -0800

    Add a new query method to query all articles for ArticleStatus

--- Backend/src/main/java/com/cisco/TrendSight/repository/ArticleRepository.java
+++ Backend/src/main/java/com/cisco/TrendSight/repository/ArticleRepository.java
@@ -1,5 +1,6 @@
 package com.cisco.TrendSight.repository;
 
+import com.cisco.TrendSight.model.ArticleStatus;
 import com.cisco.TrendSight.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>{
     List<Article> findAllByMyUser(MyUser myUser);
+    List<Article> findAllByArticleStatus(ArticleStatus articleStatus);
 }
