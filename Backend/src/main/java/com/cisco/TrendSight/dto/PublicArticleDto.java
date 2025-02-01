b1f281b977370f1e855c02e31dd85728fc332a1d
alexa <alexanderlee014@gmail.com>
2025-01-31 18:27:55 -0800

    Customize PublicArticleDto to display only necessary information for readers/non account users to see from the articles

--- Backend/src/main/java/com/cisco/TrendSight/dto/PublicArticleDto.java
+++ Backend/src/main/java/com/cisco/TrendSight/dto/PublicArticleDto.java
@@ -1,16 +1,21 @@
 package com.cisco.TrendSight.dto;
 
-public class ArticleDto {
+/** This class is used to create objects that all users/readers to see Articles
+ *  This should be restricted article data such as Title, Body, Author, Date
+ */
+public class PublicArticleDto {
     private String title;
     private String body;
     private String author;
+    private long dateInEpochMS;
 
-    public ArticleDto(){}
+    public PublicArticleDto(){}
 
-    public ArticleDto(String title, String body, String author) {
-        this.body = body;
+    public PublicArticleDto(String title, String author, String body, long dateInEpochMS) {
         this.title = title;
         this.author = author;
+        this.body = body;
+        this.dateInEpochMS = dateInEpochMS;
     }
 
     public String getTitle() {
@@ -35,5 +40,13 @@ public class ArticleDto {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public long getDateInEpochMS() {
+        return dateInEpochMS;
+    }
+
+    public void setDateInEpochMS(long dateInEpochMS) {
+        this.dateInEpochMS = dateInEpochMS;
     }
 }
