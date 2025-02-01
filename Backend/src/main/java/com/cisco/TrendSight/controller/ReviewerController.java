9b082cfaadd60018ae269b177498b614cf9c4544
alexa <alexanderlee014@gmail.com>
2025-01-31 18:27:09 -0800

    Updated reviewer endpoints and reviewArticle to include ReviewName after Reviewing Article

--- Backend/src/main/java/com/cisco/TrendSight/controller/ReviewerController.java
+++ Backend/src/main/java/com/cisco/TrendSight/controller/ReviewerController.java
@@ -1,17 +1,21 @@
 package com.cisco.TrendSight.controller;
 
-import com.cisco.TrendSight.dto.ArticleReviewDto;
+import com.cisco.TrendSight.dto.ArticleReviewDataDto;
+import com.cisco.TrendSight.dto.AuthorArticleDto;
 import com.cisco.TrendSight.model.Article;
 import com.cisco.TrendSight.model.ArticleStatus;
 import com.cisco.TrendSight.repository.ArticleRepository;
-import com.cisco.TrendSight.repository.MyUserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
+@EnableMethodSecurity
 @RestController
 public class ReviewerController {
 
@@ -21,12 +25,14 @@ public class ReviewerController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/review/article")
+    @PreAuthorize("hasRole('REVIEWER')")
+    @GetMapping("/article/review")
     public ResponseEntity<List<Article>> getPendingArticles(){
         return ResponseEntity.ok(articleRepository.findAllByArticleStatus(ArticleStatus.PENDING));
     }
 
-    @GetMapping("/review/article/{id}")
+    @PreAuthorize("hasRole('REVIEWER')")
+    @GetMapping("/article/review/{id}")
     public ResponseEntity<Article> getPendingArticle(@PathVariable long id){
         if(articleRepository.findById(id).isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -35,16 +41,19 @@ public class ReviewerController {
         return new ResponseEntity<>(article, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/review/article/{id}")
-    public ResponseEntity<Article> reviewArticle(@PathVariable long id, @RequestBody ArticleReviewDto reviewerComments){
+    @PreAuthorize("hasRole('REVIEWER')")
+    @PostMapping("/article/review/{id}")
+    public ResponseEntity<Article> reviewArticle(@PathVariable long id, @RequestBody ArticleReviewDataDto reviewDataDto){
 
         Optional<Article> articleOptional = articleRepository.findById(id);
         if(articleOptional.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         Article article = articleOptional.get();
-        article.setArticleStatus(reviewerComments.getArticleStatus());
-        article.setCommentBody(reviewerComments.getCommentBody());
+        article.setArticleStatus(reviewDataDto.getArticleStatus());
+        article.setReviewerName(reviewDataDto.getReviewerName());
+        article.setCommentBody(reviewDataDto.getCommentBody());
+        article.setReviewedDateInEpochMS(Instant.now().toEpochMilli());
         articleRepository.save(article);
         return new ResponseEntity<>(article,HttpStatus.OK);
     }
